de7955663aa38528408acf08b3e5f04cdc975685
mrQBS2 <bkadyrzhanov@mtcom.kz>
2024-05-27 16:24:10 +0500

    added goods to cart popup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa"
+import Order from './Order'
 
-export default function Header() {
+const showOrders = (props) => {
+  return(<div>
+    {props.orders.map(el=> (
+      <Order key={el.id} item={el} />
+    ))}
+  </div>)
+}
+
+const showNothing = () => {
+  return(<div className='empty'>
+    <h2>empty</h2>
+  </div>)
+}
+
+export default function Header(props) {
   let[cartOpen, setCartOpen]=useState(false)
 
 
@@ -15,6 +30,14 @@ export default function Header() {
               <li>My Account</li>
             </ul>
             <FaShoppingCart onClick={()=>setCartOpen(cartOpen=!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+
+            {cartOpen && (
+              <div className='shop-cart'> 
+                {props.orders.length >0 ?
+                  showOrders(props): showNothing()}
+              </div>
+            )}
         </div>
         <div className='presentation'></div>
     </header>
